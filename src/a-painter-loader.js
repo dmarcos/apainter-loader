5281262f801ed8ddb5c2321ec4298356a9b910ce
Thomas Balouet <t.balouet@gmail.com>
2017-05-22 13:47:47 +0200

    changed model-painted to model-loaded for better comprehension

--- src/a-painter-loader.js
+++ src/a-painter-loader.js
@@ -128,7 +128,7 @@ AFRAME.registerComponent('a-painter-loader', {
       } else {
         self.loadJSON(JSON.parse(buffer));
       }
-      self.el.emit('model-painted', {});
+      self.el.emit('model-loaded', {});
     });
   }
 
